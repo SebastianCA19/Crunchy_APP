463a3382ebc3dcef2d7bb50310b9ee54adf3a764
vladimir <vladimir_6606@hotmail.com>
2025-03-09 19:00:57 -0500

    Add history functionality and admin fragment updates
    -   Added a new `HistoryActivity` to display a list of previous orders.
    -   Created a `Compra` class to represent individual orders.
    -   Implemented `CompraAdapter` to manage the order data in a `RecyclerView`.
    -   Created layouts for the history activity (`activity_history.xml`) and individual order items (`item_historial.xml`).
    -   Added a button to navigate to the `HistoryActivity` from the `AdminFragment`.
    -   Modified `AdminFragment` to include product names and quantities, and profits.
    -   Added a new drawable `baseline_attach_money_24.xml` for visual enhancement.
    -   Updated `MainActivity` to navigate to the home screen.
    -   Added the new `HistoryActivity` to the `AndroidManifest.xml`.

--- app/src/main/java/com/example/crunchy_app/MainActivity.java
+++ app/src/main/java/com/example/crunchy_app/MainActivity.java
@@ -22,9 +22,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
     }
     public void goToHome(View v){
         Intent intent = new Intent(this, Home.class);
         startActivity(intent);
     }
+
 }
